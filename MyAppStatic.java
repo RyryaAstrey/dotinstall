0e2e9d0b182389a6c1e54f427a179c0262ee97eb
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-04-28 17:08:04 +0900

    staticの学習まで完了

---
+++ MyAppStatic.java
@@ -0,0 +1,23 @@
+class User {
+    private String name;
+    private static int count = 0;
+
+    public User(String name) {
+        this.name = name;
+        User.count++;
+    }
+
+    public static void getInfo() {
+        System.out.println("# of instances: " + User.count);
+    }
+}
+
+public class MyAppStatic {
+    public static void main(String[] args) {
+        User.getInfo();
+        User tom = new User("tom");
+        User.getInfo();
+        User bob = new User("bob");
+        User.getInfo();
+    }
+}
